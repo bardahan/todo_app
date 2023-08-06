cc9b49984e8fb8385152665c51fa886b41b3bc49
Daniel Oleynyk <danielo@hopon.co.il>
2023-08-06 21:20:26 +0300

    updated dockerfiles

--- back/settings.py
+++ back/settings.py
@@ -1,7 +1,7 @@
 import os
 
 
-DB_HOST = os.getenv("DB_HOST", "database-1.csqwyiwvwe6r.eu-central-1.rds.amazonaws.com")
-DB_PASS = os.getenv("DB_PASS", "0Password")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_SCHEME = os.getenv("DB_SCHEME", "postgres")
+DB_HOST = os.getenv("DB_HOST")
+DB_PASS = os.getenv("DB_PASS")
+DB_USER = os.getenv("DB_USER")
+DB_SCHEME = os.getenv("DB_SCHEME")
